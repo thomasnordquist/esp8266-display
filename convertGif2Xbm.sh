ddf43795f444a14bf5ef1a5addac47c948665c3d
Thomas Nordquist <thomas.nordquist@viison.com>
2016-12-09 20:31:20 +0100

    Fix gif conversion tool

--- convertGif2Xbm.sh
+++ convertGif2Xbm.sh
@@ -5,7 +5,7 @@ THRESHOLD=$2
 OUTPUT_FILE=$3.h
 NAME=`echo $3 | awk '{ print toupper($1) }'` 
 
-convert "$FILE" -coalesce -threshold $THRESHOLD"%" -background white -alpha remove $NAME.xbm
+convert "$FILE" -coalesce -threshold $THRESHOLD"%" -background white -alpha remove -resize 128x64 $NAME.xbm
 COUNT=`ls -1 $NAME-*.xbm | wc -l`
 
 #asseble file
@@ -21,16 +21,16 @@ echo "#define "$NAME"_FRAMES" $COUNT >> $OUTPUT_FILE
 # Create an array with all char arrays
 IFS=$'\n'
 ARRAY="const char* $NAME"'[] = {'
-for LINE in `cat $OUTPUT_FILE | grep "static"`; do
-	ARRAY="$ARRAY `echo -n $LINE | awk '{print $3}' | cut -d[ -f1`,"
+for LINE in `cat $OUTPUT_FILE | grep "static" | awk '{print $3}' | sort -n -t_ -k 2`; do
+	ARRAY="$ARRAY `echo -n $LINE | cut -d[ -f1`,"
 done;
 
 ARRAY=$ARRAY" };";
 
 echo $ARRAY >> $OUTPUT_FILE
 
-WIDTH=`cat $OUTPUT_FILE | grep 0_width | awk '{print $3}'`
-HEIGHT=`cat $OUTPUT_FILE | grep 0_height | awk '{print $3}'`
+WIDTH=`cat $OUTPUT_FILE | grep 0_width |tail -n1 | awk '{print $3}'`
+HEIGHT=`cat $OUTPUT_FILE | grep 0_height | tail -n1 | awk '{print $3}'`
 
 echo "#define $NAME""_WIDTH $WIDTH" >> $OUTPUT_FILE
 echo "#define $NAME""_HEIGHT $HEIGHT" >> $OUTPUT_FILE
